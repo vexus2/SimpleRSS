f70ec99dd45bf9889303f89ccbb343e6af1eae5d
hikaru.tooyama <hikaru.tooyama@nanapi.co.jp>
2014-01-28 09:06:07 +0900

    initial commit

---
+++ AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SimpleRSS
+//
+//  Created by tooyama on 2014/01/28.
+//  Copyright (c) 2014 vexus2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MasterViewController.h
@@ -0,0 +1,15 @@
+//
+//  MasterViewController.h
+//  SimpleRSS
+//
+//  Created by tooyama on 2014/01/28.
+//  Copyright (c) 2014 vexus2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface MasterViewController : UITableViewController
+
+
+@end
